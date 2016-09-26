d97174f26ae065e8a74e8b6c1e89bf2025259e1d
crispybee <crispybee9@gmail.com>
2016-09-26 18:49:11 +0200

    added navbar component

---
+++ app/navBar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+
+@Component({
+    selector: 'navbar-component',
+    templateUrl: 'app/html/navbar.html'
+})
+export class NavBarComponent {
+    navBarTitle: string;
+    
+    constructor() {
+        this.navBarTitle = "Patient";
+    }
+}
