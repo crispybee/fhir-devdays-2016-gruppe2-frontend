748bf75a84b622cde99847a70588065a408d405e
crispybee <crispybee9@gmail.com>
2016-09-26 18:48:15 +0200

    added canvas component

---
+++ app/canvas.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+
+@Component({
+    selector: 'canvas-component',
+    templateUrl: 'app/html/canvas.html'
+})
+export class CanvasComponent {
+    canvasTitle: string;
+    sectionTitle: string;
+    
+    constructor() {
+        this.canvasTitle = "Blood sugar";
+        this.sectionTitle = "Latest blood sugar values";
+    }
+}
