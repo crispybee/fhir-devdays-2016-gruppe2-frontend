1312e7a68a471b50001daa04d4778964f36cf768
crispybee <crispybee9@gmail.com>
2016-09-26 16:42:47 +0200

    added FhirProvider to module

--- app/app.module.js
+++ app/app.module.js
@@ -12,12 +12,14 @@ var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var app_component_1 = require('./app.component');
 var sidebarList_component_1 = require('./sidebarList.component');
+var fhirProvider_service_1 = require('./fhirProvider.service');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
             imports: [platform_browser_1.BrowserModule],
+            providers: [fhirProvider_service_1.FhirProvider],
             declarations: [app_component_1.AppComponent, sidebarList_component_1.SidebarListComponent],
             bootstrap: [app_component_1.AppComponent, sidebarList_component_1.SidebarListComponent]
         }), 
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } 		from '@angular/core';
 import { BrowserModule } 	from '@angular/platform-browser';
 import { AppComponent } 	from './app.component';
 import { SidebarListComponent } 	from './sidebarList.component';
+import { FhirProvider } from './fhirProvider.service';
 
 @NgModule({
 	imports: [ BrowserModule ],
+	providers: [ FhirProvider ],
 	declarations: [ AppComponent, SidebarListComponent ],
 	bootstrap: [ AppComponent, SidebarListComponent ]
 })
