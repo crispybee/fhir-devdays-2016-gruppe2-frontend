66e4df117b3630e9732144b7da8285e560033e37
crispybee <crispybee9@gmail.com>
2016-09-26 18:51:36 +0200

    splitted the design into 4 components and link them with angular

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } 		from '@angular/core';
 import { BrowserModule } 	from '@angular/platform-browser';
 import { AppComponent } 	from './app.component';
 import { SidebarListComponent } 	from './sidebarList.component';
+import { NavBarComponent } 	from './navBar.component';
+import { CanvasComponent } 	from './canvas.component';
 import { FhirProvider } from './fhirProvider.service';
 
 @NgModule({
 	imports: [ BrowserModule ],
 	providers: [ FhirProvider ],
-	declarations: [ AppComponent, SidebarListComponent ],
-	bootstrap: [ AppComponent, SidebarListComponent ]
+	declarations: [ AppComponent, SidebarListComponent, NavBarComponent, CanvasComponent ],
+	bootstrap: [ AppComponent, SidebarListComponent, NavBarComponent, CanvasComponent ]
 })
 export class AppModule {
 
