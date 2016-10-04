7a589363daef612658182e44559146fbe3d0c564
crispybee <crispybee9@gmail.com>
2016-10-04 18:11:00 +0200

    added pageNotFound component

---
+++ app/pageNotFound.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'page-not-found-component',
+    templateUrl: 'app/html/notfound.html'
+})
+export class PageNotFoundComponent {
+
+    constructor() {
+
+    }
+}
