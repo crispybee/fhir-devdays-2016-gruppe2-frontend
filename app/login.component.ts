2436ef10af386838f5ca22d288628d377541cac0
Jenshika Yoganathan <jyoganat@stud.hs-heilbronn.de>
2016-10-07 21:35:19 +0200

    Clean up login.html,
    added imports to  login component

--- app/login.component.ts
+++ app/login.component.ts
@@ -1,3 +1,11 @@
-export class LoginComponent{
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+    selector: 'login-component',
+    templateUrl: 'app/html/login.html'
+})
+
+export class LoginComponent {
 
 }
