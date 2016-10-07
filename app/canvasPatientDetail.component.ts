ad6535fbb47225c4d37c8a5940e294c3310448a1
crispybee <crispybee9@gmail.com>
2016-10-07 22:40:46 +0200

    add CanvasDetail component

---
+++ app/canvasPatientDetail.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+	selector: 'canvas-detail-component',
+	templateUrl: 'app/html/canvasDetail.html'
+})
+export class CanvasPatientDetailComponent {
+	canvasDetailTitle: string;
+	sectionTitle: string;
+	data: string[] = [
+		'Bla',
+		'Blub',
+		'Bleb',
+		'Blib'
+	];
+
+	constructor() {
+		this.canvasDetailTitle = "Blood sugar";
+		this.sectionTitle = "Latest blood sugar values";
+	}
+}
