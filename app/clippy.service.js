62581b7d18e55265b57fb4dbccd8d2a5f9d3acdf
crispybee <crispybee9@gmail.com>
2016-11-16 15:57:56 +0100

    added clippy text

--- app/clippy.service.js
+++ app/clippy.service.js
@@ -12,8 +12,20 @@ var core_1 = require("@angular/core");
 var Clippy = (function () {
     function Clippy() {
         this.importantInformation = [
-            "",
-            ""
+            "It is FHIR alarm, we call it a burnt area",
+            "SMART is the hot news",
+            "Bald ist FHIRabend",
+            "Irish Interoperability Projects - FHIReland",
+            "FHIR!I am the god of hell fire and I bring you:\n\t\tFHIR!\n\t\tI'll take you to burn.\n\t\tFHIR!\n\t\tI'll take you to learn.\n\t\tI'll see you burn!",
+            "Is it hot in here? Wait...it's just me...",
+            "Set a hospital on FHIR and it will be hot",
+            "....and it burns, burns, burns...the ring of FHIR....the ring of FHIR....",
+            "A FHIR cannot kill a dragon!",
+            "Daenerys stormborn of the house Targaryen, the first of her name, the unburnt queen of meereen, queen of the andals and the rhoynar and the FHIR",
+            "Everyone in the jury will get 50€ if we win :)",
+            "the hotter you are, the faster we come",
+            "friendly FHIR",
+            "Don't play with FHIR"
         ];
         clippy.load('Clippy', function (agent) {
             agent.show();
--- app/clippy.service.ts
+++ app/clippy.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from "@angular/core";
+import integer = fhir.integer;
 
 declare var clippy: any;
 
@@ -7,8 +8,25 @@ declare var clippy: any;
 export class Clippy {
 
 	importantInformation: string[] = [
-		"",
-		""
+		"It is FHIR alarm, we call it a burnt area",
+		"SMART is the hot news",
+		"Bald ist FHIRabend",
+		"Irish Interoperability Projects - FHIReland",
+		`FHIR!I am the god of hell fire and I bring you:
+		FHIR!
+		I'll take you to burn.
+		FHIR!
+		I'll take you to learn.
+		I'll see you burn!`,
+		"Is it hot in here? Wait...it's just me...",
+		"Set a hospital on FHIR and it will be hot",
+		"....and it burns, burns, burns...the ring of FHIR....the ring of FHIR....",
+		"A FHIR cannot kill a dragon!",
+		"Daenerys stormborn of the house Targaryen, the first of her name, the unburnt queen of meereen, queen of the andals and the rhoynar and the FHIR",
+		"Everyone in the jury will get 50€ if we win :)",
+		"the hotter you are, the faster we come",
+		"friendly FHIR",
+		"Don't play with FHIR"
 	];
 
 
